8947cade2fcaedd9538193fb7154544f5e780271
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2025-01-02 01:27:26 +0000

    Update chain-utils.ts

--- src/lib/chain-utils.ts
+++ src/lib/chain-utils.ts
@@ -1,9 +1,9 @@
-export const TELOSCAN_MAINNET_URL = 'https://teloscan.io';
+export const TELOSCAN_MAINNET_URL = 'https://rpc.trigonevm.space';
 export const TELOSCAN_TESTNET_URL = 'https://testnet.teloscan.io';
-export const BETA_TELOSCAN_MAINNET_URL = 'https://beta.teloscan.io';
+export const BETA_TELOSCAN_MAINNET_URL = 'https://https://929.rpc.thirdweb.com';
 export const BETA_TELOSCAN_TESTNET_URL = 'https://beta.testnet.teloscan.io';
 
-export const TELOS_MAINNET_CHAIN_ID = 40;
+export const TELOS_MAINNET_CHAIN_ID = 7171;
 export const TELOS_TESTNET_CHAIN_ID = 41;
 
 const currentChainId = Number(process.env.NETWORK_EVM_CHAIN_ID);
